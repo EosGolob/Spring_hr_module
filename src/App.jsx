a81a2c63f463dc940fcf4421192ebc81b83af75c
Amrit9472 <amr.raj112@gmail.com>
2024-06-02 22:53:11 +0530

    Addition of form field

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import EmployeeCreateComponent from './components/EmployeeCreateComponent'
+import EmployeeCreatePageComponent from './components/EmployeeCreatePageComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={<ListEmployeeComponent/>} />
       <Route path='/add-employee' element = {<EmployeeCreateComponent/>}></Route>
       <Route path = '/edit-employee/:id' element = {<EmployeeCreateComponent/>}></Route>
+      <Route path='/add-employee2' element = {<EmployeeCreatePageComponent/>}></Route>
     </Routes>
     </BrowserRouter>
     </>
---
+++ src/components/EducationalDetailsComponent.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const EducationalDetailsComponent= ({formData , handleChange}) => {
+  return (
+    <div>
+      <form>
+      <div className="form-group">
+                <label className="form-label">Qualification</label>
+                <input
+                  type="text"
+                  placeholder="Enter qualification"
+                  className={`form-control ${errors.qualification} ?'is-invalid':''}`}
+                  value={qualification}
+                  onChange={(e) => setQualification(e.target.value)}
+                />
+                {errors.qualification && (
+                  <div className="invalid-feedback">{errors.qualification}</div>
+                )}
+              </div>
+              <div className="form-group">
+                <label className="form-label">Previous Organisation</label>
+                <input
+                  type="text"
+                  placeholder="Enter Previous Organisation"
+                  className={`form-control ${errors.previousOrganisation} ?'is-invalid':''}`}
+                  value={previousOrganisation}
+                  onChange={(e) => setPreviousOrganisation(e.target.value)}
+                />
+                {errors.previousOrganisation && (
+                  <div className="invalid-feedback">
+                    {errors.previousOrganisation}
+                  </div>
+                )}
+              </div>
+              <div className="form-group">
+                <label className="form-label">Languages</label>
+                <input
+                  type="text"
+                  placeholder="Enter languages"
+                  className={`form-control ${errors.languages} ?'is-invalid':''}`}
+                  value={languages}
+                  onChange={(e) => setLanguages(e.target.value)}
+                />
+                {errors.languages && (
+                  <div className="invalid-feedback">{errors.languages}</div>
+                )}
+              </div>
+              <div className="form-group">
+                <label className="form-label">Total Experience</label>
+                <input
+                  type="text"
+                  placeholder="Enter Total Experience"
+                  className={`form-control ${errors.experience} ?'is-invalid':''}`}
+                  value={experience}
+                  onChange={(e) => setExperience(e.target.value)}
+                />
+                {errors.experience && (
+                  <div className="invalid-feedback">{errors.experience}</div>
+                )}
+              </div>
+      </form>
+    </div>
+  )
+}
+
+export default EducationalDetailsComponent;
