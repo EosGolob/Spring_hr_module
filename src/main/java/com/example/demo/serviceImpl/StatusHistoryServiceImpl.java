1b35bc6d9e141452824a61321f802c6767ef9629
Amrit9472 <amr.raj112@gmail.com>
2024-06-03 18:15:13 +0530

    status history

--- src/main/java/com/example/demo/serviceImpl/StatusHistoryServiceImpl.java
+++ src/main/java/com/example/demo/serviceImpl/StatusHistoryServiceImpl.java
@@ -8,49 +8,52 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.entity.StatusHistory;
+import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.repository.StatusHistoryRepository;
 import com.example.demo.service.StatusHistoryService;
 
 @Service
 public class StatusHistoryServiceImpl implements StatusHistoryService {
-	
+
 	@Autowired
-    private StatusHistoryRepository statusHistoryRepository;
-	
-	
-	
+	private StatusHistoryRepository statusHistoryRepository;
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
 	@Override
 	public void createInitialStatus(Employee employee) {
-		// TODO Auto-generated method stub
-		    
-		    StatusHistory initialStatus = new StatusHistory();
-		    initialStatus.setId(generateUniqueId());
-	        initialStatus.setEmployee(employee);
-	        initialStatus.setStatus("Active");
-	        
-	        initialStatus.setChangesDateTime(LocalDateTime.now());
-	        statusHistoryRepository.save(initialStatus);
-		
+
+		StatusHistory initialStatus = new StatusHistory();
+		initialStatus.setId(generateUniqueId());
+		initialStatus.setEmployee(employee);
+		initialStatus.setStatus("Active");
+		initialStatus.setChangesDateTime(LocalDateTime.now());
+		statusHistoryRepository.save(initialStatus);
+
+//	        employee.setStatus(null);
+
 	}
 
 	private Long generateUniqueId() {
-		// TODO Auto-generated method stub
-		   return UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+		return UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
 	}
 
 	@Override
-	public void trackStatusChange(Employee employee) {
-		// TODO Auto-generated method stub
-		 StatusHistory latestStatusHistory = statusHistoryRepository.findTopByEmployeeOrderByChangesDateTimeDesc(employee);
+	public void trackStatusChange(Employee employee, String newStatus) {
 
-	        if (latestStatusHistory != null && !latestStatusHistory.getStatus().equals(employee.getStatus())) {
-	            StatusHistory statusHistory = new StatusHistory();
-	            statusHistory.setEmployee(employee);
-	            statusHistory.setStatus(employee.getStatus());
-	            statusHistory.setChangesDateTime(LocalDateTime.now());
-	            statusHistoryRepository.save(statusHistory);
-	        }
-		
+		StatusHistory latestStatusHistory = statusHistoryRepository
+				.findTopByEmployeeOrderByChangesDateTimeDesc(employee);
+		if (latestStatusHistory != null && !latestStatusHistory.getStatus().equals(newStatus)) {
+			StatusHistory statusHistory = new StatusHistory();
+			statusHistory.setEmployee(employee);
+			statusHistory.setStatus(newStatus);
+			statusHistory.setChangesDateTime(LocalDateTime.now());
+			statusHistoryRepository.save(statusHistory);
+			employeeRepository.save(employee);
+			employee.setStatus(newStatus);
+			employeeRepository.save(employee);
+		}
+
 	}
 
 }
